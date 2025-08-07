a37e326ffc51ec76b9fff2d1227095c587882cd6
Daniel Morgan <dan@danmorgan.net>
2025-08-06 17:03:18 -0700

    WIP

--- Orchis.js
+++ Orchis.js
@@ -1,13 +1,47 @@
 import mustache from 'mustache';
 import { exec } from 'node:child_process';
+import prompt from 'prompt-sync';
 
 const orchis = (executionDoc, params = {}) => new Promise((resolve, reject) => {
     if (!executionDoc) return reject('No document loaded. Aborting.');
 
+    const envalue = (input) => {
+        console.log('rrr', { ...params, ...registry });
+        const subsubDoc = JSON.parse(mustache.render(JSON.stringify(executionDoc), { ...params, ...registry }));
+        console.log('ssd', input, registry, subsubDoc);
+        if (subsubDoc.inputs[input].substr(0, 5) === "eval ") {
+            console.log('i', subsubDoc.inputs[input].substr(5));
+            const result = new Function(subsubDoc.inputs[input].substr(5))();
+            return result;
+        } else {
+            return value;
+        }
+    }
+
     const started = new Set();
     const completed = new Set();
-    const registry = {};
+    const registry = {
+        inputs: {},
+    };
     let openJobs = 0;
+
+    // Require inputs    
+    const subDoc = executionDoc;
+    if (subDoc?.inputs) {
+        //Prompt for any inputs that are missing        
+        Object.keys(subDoc.inputs).forEach((input) => {
+            subDoc.inputs = JSON.parse(mustache.render(JSON.stringify(executionDoc, registry))).inputs;
+            if (!params[input]) {
+                console.log('okkk', typeof subDoc["inputs"][input]);
+                if (typeof subDoc["inputs"][input] === 'string') {
+                    registry["inputs"][input] = envalue(input);
+                } else if (subDoc["inputs"][input]?.prompt) {
+                    const answer = prompt()(subDoc.inputs[input].prompt + ": ");
+                    registry["inputs"][input] = answer;
+                }
+            }
+        });
+    }
 
     const step = async (depth = 1) => {
         const doc = JSON.parse(mustache.render(JSON.stringify(executionDoc), params));
@@ -20,8 +54,8 @@ const orchis = (executionDoc, params = {}) => new Promise((resolve, reject) => {
         for (const [name, job] of readyJobs) {
             started.add(name);
             openJobs++;
-
             const command = JSON.parse(mustache.render(JSON.stringify(executionDoc), { ...params, ...registry })).jobs[name].command;
+            console.log(">", command);
             exec(command, async (error, stdout) => {
                 openJobs--;
                 completed.add(name);
